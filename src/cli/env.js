af6f213c7d0d5b2a45c6303e538829d786dbfe2f
lshuliak <zeleny777@gmail.com>
2022-11-25 18:42:54 +0300

    fix: main methods

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,8 +1,7 @@
 export const parseEnv = () => {
-  console.log(process.env)
   const envs = Object.keys(process.env)
-      .filter((name) => name.includes("RSS_"))
-      .map((name) => `${name}=${process.env[name]}`);
+    .filter((name) => name.includes("RSS_"))
+    .map((name) => `${name}=${process.env[name]}`);
   console.log(envs.join("; "));
 };
-parseEnv()
+parseEnv();
